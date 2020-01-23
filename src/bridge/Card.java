25ab98d35effae253fad7c2529fe37a1e5966d74
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2020-01-23 19:52:51 +0100

    Update Card.java

--- src/bridge/Card.java
+++ src/bridge/Card.java
@@ -1,39 +1,38 @@
 package bridge;
 
-import bridge.Suit.Color;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Card {
 	protected String c;
 	protected Rank rank = new Rank();
-	protected Suit.Color color;
+	protected String color;
 	protected ArrayList<Card> cards = new ArrayList<Card>(52);
 	
 	public Card() {
 		initialize();
 	}
 	
-	public Card(String c, Suit.Color color) {
+	public Card(String c, String color) {
 		this.c = c;
 		this.color = color;
 	}
 	
 	private void initialize() {
 		for(int i = 0; i < 13; i++) {
-			cards.add(new Card(rank.value[i], Color.SPADE));
+			cards.add(new Card(rank.value[i], "SPADE"));
 		}
 		
 		for(int i = 13; i < 26; i++) {
-			cards.add(new Card(rank.value[i - 13], Color.HEART));
+			cards.add(new Card(rank.value[i - 13], "HEART"));
 		}
 		
 		for(int i = 26; i < 39; i++) {
-			cards.add(new Card(rank.value[i - 26], Color.CLUB));
+			cards.add(new Card(rank.value[i - 26], "CLUB"));
 		}
 		
 		for(int i = 39; i < 52; i++) {
-			cards.add(new Card(rank.value[i - 39], Color.DIAMOND));
+			cards.add(new Card(rank.value[i - 39], "DIAMOND"));
 		}
 	}
 	
